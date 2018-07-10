8f9838299f0564ddf9a9ad1268b5752dc4a5c7d9
Fan Shiwei <fanshiwei@vizvision.com>
2018-07-10 16:06:34 +0800

    init commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "widgetvideotargetmark.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    WidgetVideoTargetMark w;
+    w.show();
+
+    return a.exec();
+}
